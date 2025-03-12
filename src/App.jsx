f0a69a932aaeb7b142f180fa90863608f340dd59
Amarjeet Kaur <amangat7017@gmail.com>
2025-03-12 19:19:55 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Home from './Components/Home'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element: <Home/>
+    },
+  ])
+
+
   return (
     <div className='box-border p-0 m-0 max-w-screen'>
-      <Home/>
+       <RouterProvider router={router}/>
     </div>
   )
 }
